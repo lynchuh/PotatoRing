d550cbb7765d62df2aaaad8d0702fcfb4669dccc
lynchuh <lynchuh@gmail.com>
2019-04-09 19:21:54 +0800

    部署

--- src/config/history.ts
+++ src/config/history.ts
@@ -1,5 +1,16 @@
 import {createBrowserHistory} from 'history'
 
-const history = createBrowserHistory()
+const ENV = process.env.NODE_ENV
+let publicUrl:string = ''
+
+if(ENV === 'development'){
+  publicUrl = '/'
+}else if(ENV === 'production'){
+  publicUrl = '/PotatoRing'
+}
+
+const history = createBrowserHistory({
+  basename: publicUrl
+})
 
 export default history
