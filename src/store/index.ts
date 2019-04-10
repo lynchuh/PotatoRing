8e7b1748b213f24e71ef160262bb7f3862cc494c
lynchuh <lynchuh@gmail.com>
2019-04-10 09:13:19 +0800

    添加环境判断控制store是否加入 react-dev-tool

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,15 @@
 import { createStore,applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension';
-
 import reducer from './rootReducer'
 
-const enhance = composeWithDevTools(applyMiddleware(thunk))
-// const enhance = applyMiddleware(thunk)
+const ENV = process.env.NODE_ENV
+let enhance = applyMiddleware(thunk)
+
+if(ENV ==='production'){
+    enhance = applyMiddleware(thunk)
+}else if(ENV === 'development'){
+    enhance = composeWithDevTools(applyMiddleware(thunk))
+}
 
 export default createStore(reducer,enhance)
